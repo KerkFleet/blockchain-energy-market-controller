0b0ca4e6a1bc4d3b98af2ae5269f21ae879dece0
kerkfleet <kerkvliet21@gmail.com>
2022-04-20 15:44:11 -0500

    Update account loading

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 from brownie import DemandReduction, accounts
+from . import utils
 import os
 from dotenv import load_dotenv, find_dotenv, set_key
 
-accounts.load('test-rinkeby')
 env = find_dotenv()
 load_dotenv(env)
+utils.load_account("UTILITY_ACCOUNT")
 
 def main():
     # dr = DemandReduction.deploy({'from': accounts[0]})
